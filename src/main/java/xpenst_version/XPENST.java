8bfa475733b3ac88ba9f044099431a66162bfee9
Sanchit Wakde <47315983+Sanchitwakde@users.noreply.github.com>
2026-04-01 00:05:29 +0530

    New Improved Structure
    This is the new refactored improved project structure

--- src/main/java/xpenst_version/XPENST.java
+++ src/main/java/xpenst_version/XPENST.java
@@ -1,134 +1,17 @@
-
-
 package xpenst_version;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.sql.Connection;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.List;
 
-
-class expense{
-    double amt;
-    String itemi;
-    String category;
-    LocalDate tdate; // today date 
-    LocalTime ntime; //time of item purchased
-
-    public expense(double amt,String itemi,String category,LocalDate tdate,LocalTime ntime){
-        this.amt = amt;
-        this.itemi = itemi;
-        this.category = category;
-        this.tdate = tdate;
-        this.ntime = ntime;
-    }
-
-
-}
-class expense_tracker {
+import xpenst_version.Database.DataConnections;
+import xpenst_version.Model.Expense;
+import xpenst_version.Service.ExpenseService;
     
-    Scanner in;
-    double amt;
-    String itemi;
-    String category;
-    LocalDate date; // today date 
-    LocalTime time; //time of item purchased
-
-    public expense_tracker(Scanner in){
-        this.in = in;
-    }
-    
-    ArrayList<expense> expnselist = new ArrayList<>();
-
-    public void times(){
-        System.out.println("Do you want to enter time Y/N: ");
-            String time_choice = in.nextLine();
-            if(time_choice.equalsIgnoreCase("y")){
-                System.out.println("Enter time in Format(HH:MM)");
-                String input = in.nextLine();
-                time = LocalTime.parse(input);
-            }else{
-                time = LocalTime.now();
-                // System.out.println("no time added");
-            }
-    }
-
-
-    public void console(){
-       
-        System.out.println("Which category you want to select:\n1.Food\n2.Travel\n3.Shopping\n4.Education\n5.Loan\n6.EMI");
-        System.out.print("Enter your choice: ");
-        int choice_category = in.nextInt();
-        in.nextLine(); // to clear buffer 
-
-        switch(choice_category){
-            case 1:
-            category = "Food";
-            break;
-
-            case 2:
-            category = "Travel";
-            break;
-
-            case 3:
-            category = "Shopping";
-            break;
-            
-            case 4:
-            category = "Education";
-            break;
-
-            case 5:
-            category = "Loan";
-            break;
-
-            case 6:
-            category = "EMI";
-            break;
-
-            default:
-                System.out.println("Please choose a correct option");
-                
-        }
-        System.out.print("Enter item: ");
-        itemi = in.nextLine(); 
-
-        System.out.print("Enter Amount: ");
-        amt = in.nextDouble();
-        in.nextLine();
-        times(); // to get time when purchased 
-        date = LocalDate.now(); // to get current date 
-        expense expnse = new expense(amt,itemi,category,date,time);
-        expnselist.add(expnse);
-        savexpense(expnse); // object is passed so that it can work connection establised from this method to another 
-        
-        System.out.println("__Expense List__");
-        for(expense e : expnselist){
-            System.out.println("Item: " + e.itemi +" | "+ "Rs. "+e.amt+" | "+"Category: "+ e.category +" | "+"Today's Date: "+ e.tdate+" | "+"Time: "+e.ntime);
-        }
-    }
-    public void savexpense(expense expnse)// here expense is the class and expnse is the object that i have created inn previous class 
-    {
-        
-        try(BufferedWriter uinput = new BufferedWriter( new FileWriter("Userdata.txt",true))){
-            String convo_data = 
-                expnse.itemi+" | "+
-                expnse.amt+" | "+
-                expnse.category+" | "+
-                expnse.tdate+" | "+
-                expnse.ntime;
-            uinput.write(convo_data);
-            uinput.newLine();
-        
-        }catch(IOException e){
-            System.out.println("Something is wrong with file or permission");
-        }
-    }
-    
-    }
+   
 
 public class XPENST {
 public static void main(String[] args){
@@ -136,19 +19,82 @@ public static void main(String[] args){
             System.out.println("________Welcome to XPENST________");
             System.out.println("-> One stop destination to manage your Expenses <-");
             
-            expense_tracker et = new expense_tracker(in);
+            Connection conn = DataConnections.getConnection();
+            if(conn != null){
+                System.out.println("Database is sucessfully connected ");
+            }else{
+                System.out.println("Connection Failed");
+            }
+           
+            ExpenseService service = new ExpenseService(in);
             
             String loop_choice = "y";
             while(loop_choice.equalsIgnoreCase("y")){
-            try{  
-                et.console();
-                System.out.println("Do you want to add another item Y/N: ");
-                loop_choice = in.nextLine();
+            try{  double amt;
+            String itemi;
+            String category = "";
+
+            
+                 System.out.println("Which category you want to select:\n1.Food\n2.Travel\n3.Shopping\n4.Education\n5.Loan\n6.EMI");
+            System.out.print("Enter your choice: ");
+            int choice_category = in.nextInt();
+            in.nextLine(); // to clear buffer 
+
+            switch(choice_category){
+            case 1 -> category = "Food";
+
+            case 2 -> category = "Travel";
+
+            case 3 -> category = "Shopping";
+            
+            case 4 -> category = "Education";
+
+            case 5 -> category = "Loan";
+
+            case 6 -> category = "EMI";
+
+                default -> System.out.println("Please choose a correct option");
+                    
+            }
+            System.out.print("Enter item: ");
+            itemi = in.nextLine(); 
+
+            System.out.print("Enter Amount: ");
+            amt = in.nextDouble();
+            in.nextLine();
+
+            LocalTime time = service.getTime(); // to get time when purchased 
+            LocalDate date = service.getDate(); // to get current date 
+            
+            Expense expnse = new Expense(amt,itemi,category,date,time);
+            
+            service.addExpense(expnse);
+            service.displayExpense();
+            
+            System.out.println("\nDo you want to add another item Y/N:");
+                    loop_choice = in.nextLine();
+            
+            
+
+            System.out.println("Do you want to view all Expense History Y/N: ");
+            List<Expense> list = service.view_all();    
+                String viewhistory = in.nextLine();
+                if(viewhistory.equalsIgnoreCase( "y")){
+                for(Expense e :list){
+                System.out.println(
+                "Item: " + e.getItem()+
+                " | Rs. " + e.getAmount() +
+                " | Category: " + e.getCategory() +
+                " | Date: " + e.getDate() +
+                " | Time: " + e.getTime()
+                );
+            }
+            }
             }catch(InputMismatchException e){
                     System.out.println("Enter choose a valid option");
                 }
             }
-            System.out.println("Thank you for using XPENST\nSee you soon...");
+            System.out.println("Thank you for using XPENST ");
         }
 }
 }
