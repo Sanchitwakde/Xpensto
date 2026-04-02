e0e867bf0943f37aa164bde98f5bc8ff18f635c6
Sanchit Wakde <47315983+Sanchitwakde@users.noreply.github.com>
2026-04-02 21:49:08 +0530

    updated ui with menu
    Added menu option to the ui that make it convinient for the user do not have to do things only sequentially
    Now they can choose whatever option they want from add, update view and delete

--- src/main/java/xpenst_version/XPENST.java
+++ src/main/java/xpenst_version/XPENST.java
@@ -16,9 +16,10 @@ import xpenst_version.Service.ExpenseService;
 public class XPENST {
 public static void main(String[] args){
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("________Welcome to XPENST________");
+            System.out.println("        ________Welcome to XPENST________");
             System.out.println("-> One stop destination to manage your Expenses <-");
-            
+
+             //Connection to MySql Database 
             Connection conn = DataConnections.getConnection();
             if(conn != null){
                 System.out.println("Database is sucessfully connected ");
@@ -30,12 +31,32 @@ public static void main(String[] args){
             
             String loop_choice = "y";
             while(loop_choice.equalsIgnoreCase("y")){
-            try{  double amt;
-            String itemi;
-            String category = "";
+            try{  
+           
 
-            
-                 System.out.println("Which category you want to select:\n1.Food\n2.Travel\n3.Shopping\n4.Education\n5.Loan\n6.EMI");
+            System.out.println("""
+            ------MENU------
+            1. Add Expense 
+            2. Update Expense 
+            3. View all Expenses
+            4. Delete Expense
+            5. Exit""");
+
+            System.out.print("Enter your choice: ");
+            int menu_choice = in.nextInt();
+            in.nextLine();
+                
+            switch(menu_choice){
+            case 1 -> {
+            String category = "";
+            System.out.println("""
+                    Which category you want to select:
+                    1.Food
+                    2.Travel
+                    3.Shopping
+                    4.Education
+                    5.Loan
+                    6.EMI""");
             System.out.print("Enter your choice: ");
             int choice_category = in.nextInt();
             in.nextLine(); // to clear buffer 
@@ -53,57 +74,66 @@ public static void main(String[] args){
 
             case 6 -> category = "EMI";
 
-                default -> System.out.println("Please choose a correct option");
+            default -> System.out.println("Please choose a correct option");
                     
             }
             System.out.print("Enter item: ");
-            itemi = in.nextLine(); 
+            String item = in.nextLine(); 
 
             System.out.print("Enter Amount: ");
-            amt = in.nextDouble();
+            double amt = in.nextDouble();
             in.nextLine();
 
-            LocalTime time = service.getTime(); // to get time when purchased 
-            LocalDate date = service.getDate(); // to get current date 
+            LocalTime time = service.getTime(); // to get time 
+            LocalDate date = service.getDate(); // to get date 
             
-            Expense expnse = new Expense(amt,itemi,category,date,time);
+            Expense expnse = new Expense(amt,item,category,date,time);
             
             service.addExpense(expnse);
             service.displayExpense();
             
             System.out.println("\nDo you want to add another item Y/N:");
                     loop_choice = in.nextLine();
-            
-            
+            }
+            case 2 ->{
+                    System.out.println("---- Update Expense ----");
 
-            System.out.println("Do you want to view all Expense History Y/N: ");
-            List<Expense> list = service.view_all();    
-                String viewhistory = in.nextLine();
-                if(viewhistory.equalsIgnoreCase( "y")){
+                }
+            case 3->{
+                System.out.println("---- View All Expenses ----");
+                List<Expense> list = service.view_all();    
+                // String viewhistory = in.nextLine();
+                // if(viewhistory.equalsIgnoreCase( "y")){
                 for(Expense e :list){
                 System.out.println(
                 "Item: " + e.getItem()+
                 " | Rs. " + e.getAmount() +
                 " | Category: " + e.getCategory() +
                 " | Date: " + e.getDate() +
-                " | Time: " + e.getTime()
-                );
-            }
-            }
-            System.out.println("Do you want to delete any Expense Y/N: ");
-            String del_ans = in.nextLine();
-             if(del_ans.equalsIgnoreCase("y")){
+                " | Time: " + e.getTime());
+                }
+                }
+            case 4 ->{
+                System.out.println("---- Delete Expense ----");
+                // System.out.println("Do you want to delete any Expense Y/N: ");
+                // String del_ans = in.nextLine();
+                // if(del_ans.equalsIgnoreCase("y")){
                 System.out.println("Enter Record ID to delete: ");
                 int delete = in.nextInt();
                 service.deleteRecord(delete);
-             }else{
+                }
+                case 5 ->{
+                System.out.println("Thank you for using XPENST ");
+                return;
+                }
+                default -> System.out.println("Please choose a correct option");
+            }
+            }
+            catch(InputMismatchException e){
                 System.out.println("Please enter a valid option");
-             }
-            }catch(InputMismatchException e){
-                    System.out.println("Please enter a valid option");
                 }
             }
-            System.out.println("Thank you for using XPENST ");
+           
         }
 }
 }
